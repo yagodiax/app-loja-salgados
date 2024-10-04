ec07b81217ff6f2b82ab59281ba3ed71d85f37a6
yago dias <100420066+BalkMasterYD@users.noreply.github.com>
2024-10-04 13:54:24 -0300

    criação do projeto

---
+++ unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Buttons, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    BitBtn3: TBitBtn;
+    Panel1: TPanel;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ unit2.pas
@@ -0,0 +1,31 @@
+unit Unit2;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  ExtCtrls, Menus, Grids, ComCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Panel1: TPanel;
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
